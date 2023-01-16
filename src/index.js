4aea272c52e0c7fe5b91974cce4178053d7b1ee4
Anna Levkovich <7604442@gmail.com>
2023-01-16 15:09:17 +0300

    fix: to do reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    return Number(String(n)
+        .split('')
+        .filter((item, index, arr) => {
+            if (item === '-' || item === '0' && index === arr.length - 1) {
+                return false;
+            }
+            return true;
+        }).reverse().join(''))
+
 }
